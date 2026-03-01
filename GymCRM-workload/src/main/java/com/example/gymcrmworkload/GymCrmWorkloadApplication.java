e86d87ac1247cb8df4a563d96be6a3df07a13f78
lukaKalandadze <luka.kalandadze1980@gmail.com>
2026-03-01 16:39:01 -0600

    Microservices_task_2

--- GymCRM-workload/src/main/java/com/example/gymcrmworkload/GymCrmWorkloadApplication.java
+++ GymCRM-workload/src/main/java/com/example/gymcrmworkload/GymCrmWorkloadApplication.java
@@ -2,10 +2,8 @@ package com.example.gymcrmworkload;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 @SpringBootApplication
-@EnableDiscoveryClient
 public class GymCrmWorkloadApplication {
 
     public static void main(String[] args) {
--- GymCRM/src/main/java/com/example/gymcrm/service/WorkloadService.java
+++ GymCRM/src/main/java/com/example/gymcrm/service/WorkloadService.java
@@ -3,23 +3,20 @@ package com.example.gymcrm.service;
 import com.example.gymcrm.model.Trainer;
 import com.example.gymcrm.model.Training;
 import com.example.gymcrm.model.dto.TrainerWorkloadRequest;
-import io.github.resilience4j.circuitbreaker.annotation.CircuitBreaker;
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.http.HttpEntity;
-import org.springframework.http.HttpMethod;
+import org.springframework.jms.core.JmsTemplate;
 import org.springframework.stereotype.Service;
-import org.springframework.web.client.RestTemplate;
 
 @Service
 @Slf4j
 public class WorkloadService {
-    private final RestTemplate restTemplate;
+    private final String TRAINER_WORKLOAD_QUEUE = "trainer.workload.queue";
+    private final JmsTemplate jmsTemplate;
 
-    public WorkloadService(RestTemplate restTemplate) {
-        this.restTemplate = restTemplate;
+    public WorkloadService(JmsTemplate jmsTemplate) {
+        this.jmsTemplate = jmsTemplate;
     }
 
-    @CircuitBreaker(name = "workloadService", fallbackMethod = "fallbackMethod")
     public void notifyNewTrainerWorkload(Training training, TrainerWorkloadRequest.ActionType actionType) {
         Trainer trainer = training.getTrainer();
         TrainerWorkloadRequest trainerWorkloadRequest = TrainerWorkloadRequest.builder()
@@ -31,19 +28,7 @@ public class WorkloadService {
                 .duration(training.getDuration())
                 .actionType(actionType)
                 .build();
-
-        HttpEntity<TrainerWorkloadRequest> entity = new HttpEntity<>(trainerWorkloadRequest);
-        restTemplate.exchange(
-                "http://GymCRM-workload/workload/trainer",
-                HttpMethod.POST,
-                entity,
-                Void.class
-        );
-
-        log.info("Workload service notified successfully");
-    }
-
-    public void fallbackMethod(Training training, TrainerWorkloadRequest.ActionType actionType, Throwable e) {
-        log.error("Workload service unavailable: {}", e.getMessage());
+        jmsTemplate.convertAndSend(TRAINER_WORKLOAD_QUEUE, trainerWorkloadRequest);
+        log.info("message to workload service sent");
     }
 }
